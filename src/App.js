70a207e87833410e9e748dc4177147a9322e37db
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2021-04-25 22:15:33 -0400

    Add Poppins font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
 import Header from 'components/Header';
 // Pages
 import Home from 'pages/Home';
 import ErrorNotFound from 'pages/ErrorNotFound';
+// Fonts
+import '@fontsource/poppins/100.css';
+import '@fontsource/poppins/200.css';
+import '@fontsource/poppins/300.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+import '@fontsource/poppins/800.css';
+import '@fontsource/poppins/900.css';
 
 function App() {
   return (
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins',
+  },
+});
+
+export default theme;
