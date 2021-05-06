caaa62955d07c57fd3c17fcfe74d075ade36ff45
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2021-05-06 12:57:17 -0400

    Add marquee effect for songbar

--- src/components/SongBar/components/LeftSide.js
+++ src/components/SongBar/components/LeftSide.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { HStack, IconButton, Text, VStack, Tag } from '@chakra-ui/react';
+import he from 'he';
+import { HStack, IconButton, Text, VStack, Tag, Flex } from '@chakra-ui/react';
 import { FaChevronUp } from 'react-icons/fa';
+import './marquee.css';
 
 function LeftSide(props) {
   const authenticated = useSelector(state => state.user.authenticated);
@@ -23,12 +25,24 @@ function LeftSide(props) {
         <Text>No playlist selected</Text>
       ) : (
         <VStack align="left" spacing={0}>
-          <Text fontWeight="bold">
-            Selected playlist: {playlists?.[selectedPlaylist]?.name}
-          </Text>
-          <Text>
-            Your next song: {playlists?.[selectedPlaylist]?.queue?.[0]?.title}
-          </Text>
+          <Flex fontWeight="bold">
+            <Text mr={2}>Selected playlist:</Text>
+            <Flex w="300px" overflow="hidden">
+              <Text className="marquee" whiteSpace="nowrap">
+                {he.decode(playlists?.[selectedPlaylist]?.name ?? '')}
+              </Text>
+            </Flex>
+          </Flex>
+          <Flex>
+            <Text mr={2}>Your next song:</Text>
+            <Flex w="300px" overflow="hidden">
+              <Text className="marquee" whiteSpace="nowrap">
+                {he.decode(
+                  playlists?.[selectedPlaylist]?.queue?.[0]?.title ?? ''
+                )}
+              </Text>
+            </Flex>
+          </Flex>
         </VStack>
       )}
     </HStack>
