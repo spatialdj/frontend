97998eb85cbd2b643d5c1001f9684664994b635a
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2021-05-08 21:15:19 -0400

    Throttle vote and save

--- src/components/Vote/index.js
+++ src/components/Vote/index.js
@@ -1,48 +1,55 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { SocketContext } from 'contexts/socket';
 import { useSelector, useDispatch } from 'react-redux';
 import { clientLike, clientSave, clientDislike } from 'slices/voteSlice';
 import { Box, IconButton, ButtonGroup } from '@chakra-ui/react';
 import { IoMdThumbsUp, IoMdThumbsDown } from 'react-icons/io';
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import throttle from 'utils/throttle';
 
 function Vote() {
   const socket = useContext(SocketContext);
   const dispatch = useDispatch();
-  const vote = useSelector(state => state.vote);
-  const { clientVote, clientSaved } = vote;
-
-  const likeSong = () => {
+  const like = () => {
+    console.log('likeSong');
     socket.emit('vote', 'like');
     dispatch(clientLike());
   };
 
-  const saveSong = () => {
+  const save = () => {
     dispatch(clientSave());
   };
 
-  const dislikeSong = () => {
+  const dislike = () => {
     socket.emit('vote', 'dislike');
     dispatch(clientDislike());
   };
+  const likeSong = useRef(throttle(like, 250));
+  const saveSong = useRef(throttle(save, 250));
+  const dislikeSong = useRef(throttle(dislike, 250));
+  const vote = useSelector(state => state.vote);
+  const currentSong = useSelector(state => state.queue.currentSong);
+  const { clientVote, clientSaved } = vote;
+
+  if (!currentSong) return null;
 
   return (
     <Box position="absolute" bottom="80px">
       <ButtonGroup size="lg" isAttached>
         <IconButton
-          onClick={likeSong}
+          onClick={likeSong.current}
           colorScheme={clientVote === 'like' ? 'blue' : 'gray'}
           aria-label="Like this song"
           icon={<IoMdThumbsUp />}
         />
         <IconButton
-          onClick={saveSong}
+          onClick={saveSong.current}
           colorScheme={clientSaved ? 'yellow' : 'gray'}
           aria-label="Save this song"
           icon={clientSaved ? <AiFillStar /> : <AiOutlineStar />}
         />
         <IconButton
-          onClick={dislikeSong}
+          onClick={dislikeSong.current}
           colorScheme={clientVote === 'dislike' ? 'blue' : 'gray'}
           aria-label="Dislike this song"
           icon={<IoMdThumbsDown />}
